3264cfdcd6a680c803f341419aaeef2424a9f39a
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-06 15:55:23 +0100

    Add thumbs down & remove comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbsUp}) => {
+const Comment = ({text, votes, id, thumbsUp, thumbsDown, removeComment}) => {
   return (
   <li>
     {text} {' '}
     <span>votes: {votes}</span>
     <button onClick={() => thumbsUp(id)}>Thumb up</button>
+    <button onClick={() => thumbsDown(id)}>Thumb down</button>
+    <button onClick={() => removeComment(id)}>X</button>
   </li>
 )};
 
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -1,9 +1,11 @@
 import {connect} from 'react-redux';
 import Comment from './Comment';
-import {thumbsUp} from './action.js';
+import {thumbsUp, thumbsDown, removeComment} from './action.js';
 
 const mapDispatchToProps = dispatch => ({
-  thumbsUp: (id) => dispatch(thumbsUp(id))
+  thumbsUp: (id) => dispatch(thumbsUp(id)),
+  thumbsDown: (id) => dispatch(thumbsDown(id)),
+  removeComment: (id) => dispatch(removeComment(id))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
