098aae77bf02dfe068493d02052b0c812edafac4
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-07 14:01:17 +0100

    Add 'add comment'

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Comment from './CommentContainer';
-import {addComment} from './action.js';
 
-const CommentsList = ({comments}) => {
+const CommentsList = ({comments, addComment}) => {
 console.log('commen', comments);
 let text;
   return  (
     <div>
-    <form onSubmit={ (e) => {addComment(text); e.preventDefault(); console.log(addComment(text));}}>
-      <input type={'text'} placeholder={'Comment'} onChange={(e) => {text = e.target.value; console.log(e.target.value);}}/>
+    <form onSubmit={ (e) => {addComment(text); e.preventDefault(); }}>
+      <input type={'text'} placeholder={'Comment'} onChange={(e) => {text = e.target.value;}}/>
       <button type={'submit'}>Add Comment</button>
     </form>
     <ul>
