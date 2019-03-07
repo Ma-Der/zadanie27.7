b0eeb3f5075a1fcc762961c7cf8bc7bb9bf5b267
Maciej Derewiański <eco_derma@poczta.onet.pl>
2019-03-07 17:00:59 +0100

    Update CommentsList

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import Comment from './CommentContainer';
 
+class CommentsList extends React.Component {
+  constructor({comments, addComment}){
+    super({comments, addComment});
+    this.state  = {
+      value: ''
+    }
+  }
+
+  handleChange(e){
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.addComment(this.state.value);
+  }
+
+  render() {
+    return  (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <input type={'text'} placeholder={'Comment'} onChange={this.handleChange}/>
+        <button type={'submit'}>Add Comment</button>
+      </form>
+      <ul>
+        {this.props.comments.map(comment => <Comment key={comment.id} {...comment}/>)}
+      </ul>
+      </div>
+    );
+  }
+}
+/*
 const CommentsList = ({comments, addComment}) => {
 console.log('commen', comments);
 let text;
@@ -16,5 +50,5 @@ let text;
     </div>
   )
 }
-
+*/
 export default CommentsList;
